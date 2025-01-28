5ef398ad22c465676868a0f420c400a438efe021
ZhangJiaJiong <2640584193@qq.com>
2025-01-28 12:10:36 +0800

    Site updated: 2025-01-28 12:10:36

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/22/hello-world/","2025/01/27/如何搭建个人博客/"];function toRandomPost(){
+var posts=["2025/01/22/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
